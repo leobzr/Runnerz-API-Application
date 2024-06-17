31226ccacb5488d50beb73c6454bb8dd62e305fc
leobzr <leo72bezerra@gmail.com>
2024-06-17 12:44:26 -0300

    a

---
+++ src/main/java/com/example/runnerz/RunnerzApplication.java
@@ -0,0 +1,18 @@
+package com.example.runnerz;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnerzApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(RunnerzApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnerzApplication.class, args);
+		log.info("Application Starter Successfully.");
+	}
+
+}
